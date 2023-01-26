b6dd92586eac2bff78a593d47b1452f1c6451546
jose-Cavallini <josecavallini2301@hotmail.com>
2023-01-25 23:53:19 -0300

    Create ejercicio2.js

---
+++ sesion-01-02-mi-primer-proyecto-de-node/ejercicio2.js
@@ -0,0 +1,14 @@
+const usuario = [
+    "Jose Cavallini", 
+    24,
+    true, 
+    Date(1999, 01, 23), 
+    {
+        titulo: "el alquimista",
+        autor: "Paulo cohelo",
+        fecha: "1981",
+        url: "https://articulo.mercadolibre.com.ar/MLA-859926975-el-alquimista-paulo-coelho-edicion-con-guia-de-lectura-_JM?matt_tool=54237661&matt_word=&matt_source=google&matt_campaign_id=19554318680&matt_ad_group_id=148693974641&matt_match_type=&matt_network=g&matt_device=c&matt_creative=644675266274&matt_keyword=&matt_ad_position=&matt_ad_type=pla&matt_merchant_id=308551527&matt_product_id=MLA859926975&matt_product_partition_id=1939699602635&matt_target_id=aud-415044759576:pla-1939699602635&gclid=Cj0KCQiAw8OeBhCeARIsAGxWtUwLmOzA7bcJDRiB_0hzxc7BYzl83Sjx_qKxwUw6JeXHHFRZKMuIV4EaAgAOEALw_wcB"
+    }
+]
+
+console.log(usuario)

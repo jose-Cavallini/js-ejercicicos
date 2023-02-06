e7da4b013fb89a9e7379a576b456899796734a01
jose-Cavallini <josecavallini2301@hotmail.com>
2023-02-06 16:32:54 -0300

    Create ejerciccio.js

---
+++ sesion-06-listas/ejerciccio.js
@@ -0,0 +1,72 @@
+/**
+Crea un archivo JS que contenga las siguientes líneas
+
+    -
+    Una variable que contenga la lista de la compra(mínimo 5 elementos)
+
+    -
+    Modifica la lista de la compra y añádele "Aceite de Girasol"
+
+    -
+    Vuelve a modificar la lista de la compra eliminando "Aceite de Girasol"
+
+    -
+    Una lista de tus 3 películas favoritas(objetos con propiedades: titulo, director, fecha)
+
+    -
+    Una nueva lista que contenga las películas posteriores al 1 de enero de 2010(utilizando filter)
+
+    -
+    Una nueva lista que contenga los directores de la lista de películas original(utilizando map)
+
+    -
+    Una nueva lista que contenga los títulos de la lista de películas original(utilizando map)
+
+    -
+    Una nueva lista que concatene la lista de directores y la lista de los títulos(utilizando concat)
+
+    -
+    Una nueva lista que concatene la lista de directores y la lista de los títulos(utilizando el factor de propagación)
+*/
+
+let lista = ["arroz", "azucar", "mayonesa", "papel", "leche"]
+console.log(lista)
+
+lista.push("Aceite de Girasol", )
+console.log(lista)
+lista.pop()
+console.log(lista)
+
+let pelisfavorita = [{
+        titulo: "avatar",
+        director: "1 nose",
+        fecha: "1 nose"
+    },
+    {
+        titulo: "Mente indomable",
+        director: "2 nose",
+        fecha: "2 nose"
+    },
+    {
+        titulo: "Gladiador",
+        director: "3 nose",
+        fecha: 20200102
+    }
+]
+//filter
+const peliposterior = pelisfavorita.filter(fecha => fecha.fecha > 20200101)
+console.log(peliposterior)
+
+//map
+const pelidirector = pelisfavorita.map((valor, indice) => `${valor.director}`)
+console.log([pelidirector])
+
+const pelititulo = pelisfavorita.map((valor, indice) => `${valor.titulo}`)
+console.log([pelititulo])
+
+//concat
+const gruplist = pelidirector.concat(pelititulo)
+console.log(gruplist)
+// ...  factor de propagacion
+const gruplist2 = [...pelidirector, ...pelititulo]
+console.log(gruplist2)
